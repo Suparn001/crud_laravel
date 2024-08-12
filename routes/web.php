8eccec3616a967c3f6a971bd85f5127a2bad3543
Suparn001 <suparngautam@gmail.com>
2024-08-12 16:55:13 +0530

    optimized routes

--- routes/web.php
+++ routes/web.php
@@ -8,11 +8,11 @@ Route::get('/', function () {
 });
 
 
-Route::controller(ProductController::class)->group(function(){
-    Route::get('/products','index')->name('products.index');
-    Route::get('/products/create','create')->name('products.create');
-    Route::post('/products','store')->name('products.store');
-    Route::get('/products/{product}/edit','edit')->name('products.edit');
-    Route::put('/products/{product}','update')->name('products.update');
-    Route::delete('/products/{product}','destroy')->name('products.destroy');    
+Route::controller(ProductController::class)->prefix('products')->group(function(){
+    Route::get('/','index')->name('products.index');
+    Route::get('/create','create')->name('products.create');
+    Route::post('/','store')->name('products.store');
+    Route::get('/{product}/edit','edit')->name('products.edit');
+    Route::put('/{product}','update')->name('products.update');
+    Route::delete('/{product}','destroy')->name('products.destroy');    
 });
